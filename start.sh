612936c116adeb041196e54e1d76e113aa8f4dcc
khan89ss <brianocnner2026@atomicmail.io>
2026-08-22 12:00:43 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-set -e
+set -x
 
 export PORT="${PORT:-8080}"
+echo "PORT is: $PORT"
+
 envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+echo "nginx config generated"
+cat /etc/nginx/nginx.conf
 
 nginx &
+echo "nginx started"
+
+ls -la /app 2>&1
+which x-ui 2>&1
 
 /app/x-ui &
+echo "x-ui launch attempted, exit code: $?"
 
 wait -n
